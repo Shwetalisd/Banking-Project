07b2964d30220fc76be22c3aa81f15b886893638
Shwetali01 <shwetadesh.tech@gmail.com>
2025-12-29 13:46:06 +0530

    Update welcome message in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Secure Banking Application"
+    return "Welcome to Banking Application on EKS"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
